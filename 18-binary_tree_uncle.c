4342fd643e4efa111a3a1bcece348a33000d1edf
Luke Hackett <luke-hackett@outlook.com>
2022-08-30 23:25:54 -0700

    changed nodes

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,11 +8,14 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *parent_node;
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
-	if (node->parent->parent->left == node)
-		return (node->parent->right);
+	parent_node = node->parent;
 
-	return (node->parent->parent->left);
+	if (parent_node->parent->left = parent_node)
+		return (parent_node->parent->right);
+
+	return (parent_node->parent_left);
 }
