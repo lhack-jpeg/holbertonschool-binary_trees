ec942d8e65a903352b26e2edd95d5c62889fc338
Luke Hackett <luke-hackett@outlook.com>
2022-08-30 18:04:43 -0700

    updated styling as per betyt

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,10 +10,8 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	else
-	{
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
-	}
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
