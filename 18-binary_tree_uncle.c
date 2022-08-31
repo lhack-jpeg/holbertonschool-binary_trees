79b731c09aacd9613eaaa5c65f485f5d44ea856d
Luke Hackett <luke-hackett@outlook.com>
2022-08-30 23:20:46 -0700

    fixed proto

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,7 +6,7 @@
  *Return: pointer to uncle node, NULL otherwise
  */
 
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
